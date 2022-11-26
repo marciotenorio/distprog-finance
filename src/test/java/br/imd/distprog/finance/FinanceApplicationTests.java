63a8b2534de8d9672d5d8abaf6d3f8ceea1a39aa
Márcio Tenório <marciotenorioj@gmail.com>
2022-11-26 16:59:56 -0300

    Import project

---
+++ src/test/java/br/imd/distprog/finance/FinanceApplicationTests.java
@@ -0,0 +1,13 @@
+package br.imd.distprog.finance;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FinanceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
